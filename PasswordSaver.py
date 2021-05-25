eb7ac1856a08e61dc4b3ee57bda373bfb06e73d6
Jitesh Bagul <78952376+jitesh-cloud@users.noreply.github.com>
2021-05-25 14:34:17 +0530

    Add files via upload

---
+++ PasswordSaver.py
@@ -0,0 +1,45 @@
+import os.path
+
+def checkExistence():
+	if os.path.exists("info.txt"):
+		pass
+	else:
+		file = open("info.txt", 'w')
+		file.close()
+
+def appendNew():
+	file = open("info.txt", 'a')
+
+	print()
+	print()
+
+	userName = input("Please enter the user name: ")
+	password = input("Please enter the password here: ")
+	website = input("Please enter the website address here: ")
+
+	print()
+	print()
+
+	usrnm = "UserName: " + userName + "\n"
+	pwd = "Password: " + password + "\n"
+	web = "Website: " + website + "\n"
+
+	file.write("--------------------------------------\n")
+	file.write(usrnm)
+	file.write(pwd)
+	file.write(web)
+	file.write("--------------------------------------\n")
+	file.write("\n")
+	file.close
+
+def readPasswords():
+	file = open("info.txt", 'r')
+	content = file.read()
+	file.close()
+	print(content)
+
+ch = checkExistence()
+
+ap = appendNew()
+
+re = readPasswords()
